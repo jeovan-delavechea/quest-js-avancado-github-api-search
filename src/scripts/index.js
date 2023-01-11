8fcbcacd5f6f948cf0bdd8e5aeea37a4c326aa08
Jeovan Delavechea <jeovan.delavechea88@gmail.com>
2023-01-11 09:23:48 -0300

    simplificação do caminho dos módulos js chamados no arquivo principal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import { getUser } from "/src/scripts/services/get-user.js"
-import { getRepositories } from "/src/scripts/services/get-repositories.js"
-import { getEvents } from "/src/scripts/services/get-events.js"
-import { user } from "/src/scripts/objects/user.js"
-import { screen } from "/src/scripts/objects/screen.js"
+import { getUser } from "./services/get-user.js"
+import { getRepositories } from "./services/get-repositories.js"
+import { getEvents } from "./services/get-events.js"
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
